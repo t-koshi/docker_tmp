e05e338fa925c9d9f09e7342ac30b548f8bcfee8
t-koshi <greapflutea2@yahoo.co.jp>
2017-10-27 09:29:34 +0900

    ユーザーログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'me/dashboard'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  namespace :mypage do
+    get 'dashboard'
+  end  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
